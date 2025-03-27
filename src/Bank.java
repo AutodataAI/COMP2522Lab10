44d8c8447cbc4d0ae278fd2e75fcbe441bd77024
Ted Ip   <hip7@my.bcit.ca>
2025-03-26 23:18:24 -0700

    Bank adn BankAccount added and partially implemented.

--- src/Bank.java
+++ src/Bank.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+/**
+ * Manages a collection of BankAccount objects,
+ * allowing the addition of new accounts and
+ * management through unique account IDs.
+ *
+ * @author Ted , Mohammand
+ * @version 1.0
+ * */
 
 public class Bank {
+
+    /**
+     * Constructor to instantiate a Bank object.*/
+    public Bank() {
+    }
+
+    public void addAccount(BankAccount account) {
+        //do sth after account is added to a bank
+    }
+
 }
--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+/**
+ * Manages a bank account with functionalities
+ * such as depositing, withdrawing, and balance enquiry
+ *
+ * @author Ted , Mohammand
+ * @version 1.0
+ * */
 
 public class BankAccount {
+
+    private String accountNum;
+    private double balanceUSD;
+
+    /** Constructor of BankAccount Object.
+     * @param accountNum A string of account Number
+     * @param initialBalanceUSD initial balance of bank account in USD
+    */
+    public void BankAccount(final String accountNum, int initialBalanceUSD ) {
+        this.accountNum = accountNum;
+        this.balanceUSD = initialBalanceUSD;
+    }
+
+    /**
+     * To get balance of account in USD*/
+    private double getBalanceUSD() {
+        return this.balanceUSD;
+    }
+
+    /** T deposit into account.
+     * @param amount to deposit in USD
+     * */
+    private void deposit(final double amount) {
+        this.balanceUSD += amount;
+    }
+
+    /** To withdraw from account.
+     * @param amount to withdraw from account USD
+     * */
+    private void withdraw(final double amount) {
+        this.balanceUSD -= amount;
+    }
+
 }
